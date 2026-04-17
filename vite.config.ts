d481b98a17258f8128b7213bc1e5fff73ff10acf
technowizericabadiano <technowiz.abadiano.eric@gmail.com>
2026-04-18 03:08:53 +0800

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: './',
+  base: '/anniversary-app/',
   plugins: [react({ jsxRuntime: "automatic" })],
   resolve: {
     alias: {
